911450e5626ed99d1b24e68e89cf7e6e32c8dde9
Martin Rauscher <hades32@gmail.com>
2021-02-28 18:24:09 +0100

    phone-holder: don't go crazy with fillet
    Signed-off-by: Martin Rauscher <hades32@gmail.com>

--- CQ/phoneholder.py
+++ CQ/phoneholder.py
@@ -7,6 +7,6 @@ result = (
 	.faces("<Y").workplane().polygon(6, 50).cutThruAll()
 	.faces(">(1,0,1)").workplane(centerOption="CenterOfMass").polygon(6, 50).cutThruAll()
 	.faces(">X and >Z").workplane().move(0,50/2).rect(50,2).extrude(10)
-	.edges("|Y").fillet(0.8)
+	#.edges("|Y").fillet(0.8)
 	.faces(">(1,0,1)").workplane(centerOption="CenterOfMass").rect(27, 42).cutThruAll()
 )
